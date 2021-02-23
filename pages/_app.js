8c614dfcbed5779341a93c91c00fa24c5dbf484f
Flavia <flavia_ferri@hotmail.com>
2021-02-23 14:43:59 +0100

    fixing title

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import "../styles/globals.css";
+import Head from "next/head";
+
 import "reset-css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Flavia Ferri - Frontend Developer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
